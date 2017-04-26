13df91b05a0533b67c769ebcb88c6c8130caa785
Hayley-K <101098200@student.swin.edu.au>
2017-04-26 13:38:36 +1000

    changed menu settings and added more to easy AI

--- src/GameController.cs
+++ src/GameController.cs
@@ -79,7 +79,8 @@ public static class GameController
 
 		//Create the game
 		_theGame = new BattleShipsGame();
-
+		//edited here ---- not sure if this is the solution ----
+		// --------------- added case for easy and changed default to easy
 		//create the players
 		switch (_aiSetting) {
 			case AIOption.Medium:
@@ -88,8 +89,11 @@ public static class GameController
 			case AIOption.Hard:
 				_ai = new AIHardPlayer(_theGame);
 				break;
+			case AIOption.Easy:
+				_ai = new AIEasyPlayer (_theGame);
+				break;
 			default:
-				_ai = new AIHardPlayer(_theGame);
+				_ai = new AIEasyPlayer(_theGame);
 				break;
 		}
 
