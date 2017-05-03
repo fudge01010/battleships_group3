3bf4e3fca0c38cbe8d881fdc7bd74724bebdfe53
Hayley-K <101098200@student.swin.edu.au>
2017-05-03 10:05:05 +1000

    hopefully fixed aiEasy player

--- src/Model/AIEasyPlayer.cs
+++ src/Model/AIEasyPlayer.cs
@@ -10,22 +10,63 @@ public class AIEasyPlayer : AIPlayer
 	public AIEasyPlayer (BattleShipsGame controller) : base(controller)
 	{
 	}
+
+	private Location _nextLocation;
+
 	/// <summary>
-	/// Generates the coords.
-	/// </summary>
-	/// <param name="row">Row.</param>
-	/// <param name="column">Column.</param>
+	/// generate shots at random
+	/// param = row = row it generates
+	/// param - column = column it generates
 	protected override void GenerateCoords(ref int row, ref int column)
 	{
-		row = _Random.Next(0, EnemyGrid.Height);
-		column = _Random.Next(0, EnemyGrid.Width);
-	}
+		do {
+			if (_nextLocation != null) {
+				row = _nextLocation.Row;
+				column = _nextLocation.Column;
+				_nextLocation = null;
+			} else {
+				row = Player._Random.Next (0, EnemyGrid.Height);
+				column = _Random.Next (0, EnemyGrid.Width);
+			}
+		} while (EnemyGrid.Item (row, column) != TileView.Sea);
 
+	}
+	/// <summary>
+	/// ProcessShot does nothing with the easy AIPlayer because this player
+	/// doesn't know anything beside generating random coordinates
+	/// </summary>
+	/// <param name="row">row it shot at</param>
+	/// <param name="col">the col it shot at</param>
+	/// <param name="result">the result from the last shot</param>
 	protected override void ProcessShot(int row, int col, AttackResult result)
 	{
-		if (result.Value == ResultOfAttack.Hit)
-			GenerateCoords ();
+		if (result.Value == ResultOfAttack.Hit) {
+			switch (_Random.Next(4)) {
+			case 0:
+				_nextLocation = new Location(row + 1, col);
+				break;
+			case 1:
+				_nextLocation = new Location(row - 1, col);
+				break;
+			case 2:
+				_nextLocation = new Location(row, col + 1);
+				break;
+			case 3:
+				_nextLocation = new Location(row, col - 1);
+				break;
+			}
+
+			if (_nextLocation.Row < 0) {
+				_nextLocation.Row = 0;
+			} else if (_nextLocation.Row >= PlayerGrid.Height) {
+				_nextLocation.Row = PlayerGrid.Height - 1;
+			}
+
+			if (_nextLocation.Column < 0) {
+				_nextLocation.Column = 0;
+			} else if (_nextLocation.Column >= PlayerGrid.Width) {
+				_nextLocation.Column = PlayerGrid.Width - 1;
+			}
+		}
 	}
 }
-
-
